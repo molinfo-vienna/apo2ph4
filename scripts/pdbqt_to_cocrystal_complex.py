0c7010def69d49d5af320ea67e671099ec454ea3
Thomas Seidel <thomas.seidel@univie.ac.at>
2025-03-20 22:52:28 +0100

    Removed superfluous whitespace

--- scripts/pdbqt_to_cocrystal_complex.py
+++ scripts/pdbqt_to_cocrystal_complex.py
@@ -11,11 +11,7 @@ cmd.split_states('*')
 [cmd.alter(j, ''.join(('resn=\'', str(i), '\''))) for i, j in enumerate(cmd.get_object_list())]
 cmd.load('receptor.pdbqt')
 
-
 [cmd.save(''.join(('fragment_complex', str(i), '.pdb')), ''.join(('receptor or ', j))) for i, j in enumerate(cmd.get_object_list()[:-1])]
-
-
-
 
 with open ('../complex_list.list', 'w+') as complist: [complist.write(os.getcwd() + '/' + i + '\n') for i in sorted(os.listdir('.')) if i.startswith('fragment_complex')]
     
