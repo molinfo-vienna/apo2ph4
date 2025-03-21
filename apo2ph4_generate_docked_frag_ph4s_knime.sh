4abffc4902083885729df8f2b6bb8f921ef63574
Thomas Seidel <thomas.seidel@univie.ac.at>
2025-03-21 11:42:27 +0100

    Name shortening

--- apo2ph4_generate_docked_frag_ph4s_knime.sh
+++ apo2ph4_generate_docked_frag_ph4s_knime.sh
@@ -5,4 +5,4 @@ SCRIPTPATH=`dirname $SCRIPT`
 
 mkdir -p pharmacophores
 
-knime --launcher.suppressErrors -nosave -reset -nosplash -application org.knime.product.KNIME_BATCH_APPLICATION -workflowDir="${SCRIPTPATH}/knime_fragment_SB_pharmacophore_gen/"  -workflow.variable=var_output,"./pharmacophores/pharmacophores.pml",String -workflow.variable=var_input,"./complex_list.list",String -vmargs -Xmx32000m -Xms10048m 
+knime --launcher.suppressErrors -nosave -reset -nosplash -application org.knime.product.KNIME_BATCH_APPLICATION -workflowDir="${SCRIPTPATH}/knime_workflow/" -workflow.variable=var_output,"./pharmacophores/pharmacophores.pml",String -workflow.variable=var_input,"./complex_list.list",String -vmargs -Xmx32000m -Xms10048m 
